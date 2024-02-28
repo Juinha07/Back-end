4d6a25480536d608d8899b54580bf1d2688a5eb6
Juinha07 <124881088+Juinha07@users.noreply.github.com>
2024-02-28 11:11:21 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 2302 </title>
+</head>
+<body>
+    <h1>Meu primeiro PHP</h1>
+    <?php
+    date_default_timezone_set("America/Sao_Paulo");
+    echo"ola mundo. \u{1F30D} <br> ";   
+    echo date("d/M/y");
+    echo"<br>";
+    echo date("g/i");
+    ?>
+</body>
+</html>
