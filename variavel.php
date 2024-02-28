8e2f770bc9aeb6200d2f5fd691872e3f5c289423
Juinha07 <124881088+Juinha07@users.noreply.github.com>
2024-02-28 12:01:47 -0300

    Add files via upload

---
+++ variavel.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variavel</title>
+</head>
+<body>
+    <?php
+    $nome = "Julia";
+    const  SOBRENOME = " Rodrigues";
+    $peso=(int) 48;
+    const PESO = 48;
+    $altura = "1,60";
+    echo"  Meu nome eh $nome " . SOBRENOME;
+    echo"  peso " . PESO . " kilos";
+    echo" e tenho $altura de altura. \u{1F4A9} <br>";
+    ?>
+</body>
+</html>     
